37347d5a42f99d7c7ae240b3de606d053366cdb1
jacob solomon <jacobsolomon6@icloud.com>
2020-10-23 14:03:31 +1000

    initial commit

---
+++ sets.rb
@@ -0,0 +1,16 @@
+def intersection(set1, set2)
+    set1.each do
+            
+        # set1 and set2 are arrays
+        # return the intersection
+    end
+    
+    def union(set1, set2) 
+        # set1 and set2 are arrays
+        # return the union
+    end
+    
+    def symmetric_difference(set1, set2)
+        # set1 and set2 are arrays
+        # return the symmetric difference
+    end
---
+++ xor.rb
@@ -0,0 +1,56 @@
+
+
+def exclusive_or(num1, num2)
+
+    arr1 =[]
+    arr2 =[]
+
+    bin1 = num1.to_s(2)
+
+    bin2 = num2.to_s(2)
+
+    bin1.each_char do |c|
+        arr1 << c
+    end
+
+    bin2.each_char do |c|
+        arr2 << c
+    end
+  
+    if arr2.length < arr1.length
+        until arr2.length == arr1.length do
+        arr2.unshift(0.to_s)
+        end
+    end
+
+    if arr1.length < arr2.length
+        until arr1.length == arr2.length do
+            arr1.unshift(0.to_s)
+    end
+    end
+    
+    result = []
+
+    arr1.each_with_index do |val, i|
+        # if !arr2.include? rarr2[i]
+        #     result << val.to_i
+        # end
+        if val.to_i + arr2[i].to_i == 0
+            result << 0
+        end
+        if val.to_i + arr2[i].to_i == 2
+            result << 0
+        end
+        if val.to_i + arr2[i].to_i == 1
+            result << 1
+        end
+    
+    end
+
+    puts result.join("").to_i(2)
+
+end
+
+
+
+exclusive_or(100,11)
